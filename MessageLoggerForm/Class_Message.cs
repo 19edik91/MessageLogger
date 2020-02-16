faca5cb8779865baded16368a47dad50321a6256
Kraemere <Kraemere@31f6ca0a-dfc5-7646-ac17-90a69fb9f71f>
2020-02-16 14:31:01 +0000

    Added class for message structures
    git-svn-id: file:///C/Users/kraemere/OneDrive/SVN-Repos/VisualStudio-Projects/MessageLogger/trunk@3 31f6ca0a-dfc5-7646-ac17-90a69fb9f71f

--- MessageLoggerForm/Class_Message.cs
+++ MessageLoggerForm/Class_Message.cs
@@ -8,6 +8,89 @@ using System.Threading.Tasks;
 namespace MessageLoggerForm
 {
     class Class_Message
-    {
+    { 
+        /*************************
+        * variables and defines
+        ************************/
+        private string sMsgName;
+        private const UInt16 uiPreamble = 0x1C;
+        private UInt16 uiMsgDest = 0x10;
+        private UInt16 uiMsgFlag = 0xFFFF;     //Response is 0x80 | Request is 0x40
+        private UInt16 uiMsgSource = 0xFFFF;
+        private UInt16 uiMsgCmd = 0xFFFF;
+        private UInt16 uiMsgObj = 0xFFFF;
+        private UInt16 uiMsgSize = 0xFFFF;
+        private UInt16 uiMsgCounter = 0;
+
+
+        /****************************************************
+         * Standard object constructor
+         ***************************************************/
+        public Class_Message(string sName, UInt16 uiCmd, UInt16 uiObj, UInt16 uiSize, UInt16 uiFlag, UInt16 uiSrc, UInt16 uiDest)
+        {
+            this.sMsgName = sName;
+            this.uiMsgSource = uiSrc;
+            this.uiMsgDest = uiDest;
+            this.uiMsgCmd = uiCmd;
+            this.uiMsgObj = uiObj;
+            this.uiMsgSize = uiSize;
+            this.uiMsgFlag = uiFlag;
+        }
+
+        /****************************************************
+         * Method for returning the complete message as a string
+         ***************************************************/
+        //public override string ToString()
+        //{
+        //    return Convert.ToString(uiPreamble)+" "+ Convert.ToString(uiDest) + " " + Convert.ToString(this.uiMsgSource) + " " + Convert.ToString(this.uiMsgSize) + " "
+        //                    + Convert.ToString(this.uiMsgFlag) + " " + Convert.ToString(this.uiMsgObj) + " " + Convert.ToString(this.uiMsgCmd);
+        //}
+
+        public override string ToString()
+        {
+            /* Create new string builder object. This is more helpful for casting */
+            StringBuilder sb = new StringBuilder();
+
+            /* Format string into hex-string by using "{0:X2}" for two-digits hexadecimal */
+            //sb.AppendFormat("0x{0:X2} ", uiPreamble);
+            sb.AppendFormat("{0:X2} ", uiPreamble);
+            sb.AppendFormat("{0:X2} ", this.uiMsgDest);
+            sb.AppendFormat("{0:X2} ", this.uiMsgSource);
+            sb.AppendFormat("{0:X2} ", this.uiMsgSize);
+            sb.AppendFormat("{0:X2} ", this.uiMsgFlag);
+            sb.AppendFormat("{0:X2} ", this.uiMsgObj);
+            sb.AppendFormat("{0:X2} ", this.uiMsgCmd);
+
+            return sb.ToString();
+        }
+
+
+        /****************************************************
+         * Method for getting the payload size
+         ***************************************************/
+        public UInt16 GetPayLoadSizeString()
+        {
+            //Payload size is defined as Object + Command + Data. Therefore the payload has to be checked
+            UInt16 uiPayload = this.uiMsgSize;  //Get complete payload size (with Cmd and Obj)
+            uiPayload -= 2; //Subtract size of Cmd and Obj
+            uiPayload *= 2; //Every two characters are equal to a byte
+            uiPayload += Convert.ToUInt16((uiPayload + 1) / 2); //Add "white-spaces" for every byte
+
+            return uiPayload;
+        }
+
+        public UInt16 GetPayLoadSizeInt()
+        {
+            /* Returns the payload size (Obj + Cmd + Payload) */
+            return this.uiMsgSize;
+        }
+
+        /****************************************************
+         * Method for getting the message name
+         ***************************************************/
+        public string GetMessageName()
+        {
+            return this.sMsgName;
+        }
     }
 }
---
+++ MessageLoggerForm/MsgStructureCasted.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageLoggerForm
+{
+    public class MsgStructureCasted
+    {
+        /* This file is for the casted message structures which are used by the C# project. 
+           Booleans have to be changed into "bytes" otherwise the casting will fail! */
+
+        public struct tsMsgOutputState
+        {
+            public byte b7Brightness;           //Value in percent 0-100
+            public byte bInitMenuActiveInv;     //Inverted bit of the Init menu active bit
+            public byte bLedStatus;
+            public byte bInitMenuActive;        //Bit of the init menu active
+            public byte bAutomaticModeActive;
+        };
+
+
+        public struct tsMsgOutputStateResponse
+        {
+            public UInt16 uiVoltage;
+            public UInt16 uiCurrent;
+            public Int16 siTemperature;
+        };
+
+
+        public struct tsMsgVersion
+        {
+            public UInt16 uiVersion;
+        };
+
+
+        public struct tsMsgFaultMessage
+        {
+            public UInt16 uiErrorCode;
+        };
+
+
+        public struct tsMsgManualInit
+        {
+            public byte bSetMinValue;
+            public byte bSetMaxValue;
+        };
+
+
+        public struct tsMsgUserTimer
+        {
+            public byte ucStartHour;
+            public byte ucStopHour;
+            public byte ucStartMin;
+            public byte ucStopMin;
+            public byte b7TimerIdx;
+        };
+
+
+
+
+#warning Initialisierung des arrays notwendig!
+        public struct tsAllDlbSenseAdcResponse
+        {
+            public struct tsDlbVal
+            {
+                public Int16 siAvgVal;
+                public Int16 siMinVal;
+                public Int16 siMaxVal;
+            };
+            public tsDlbVal[] sDlbVal;
+        };
+
+        public struct tsDlbSenseAdcResponse
+        {
+            public Int16 siAvgVal;
+            public Int16 siMinVal;
+            public Int16 siMaxVal;
+            public byte ucDlbIndex;
+            public byte ucDigitalSense;
+            public byte ucSenseIdx;
+        };
+
+
+        public struct tsRawAdcValBuff
+        {
+            public Int16[] siAdcVal;
+            public Int16 siMaxVal;
+            public Int16 siMinVal;
+            public Int16 siAvgVal;
+        };
+
+#warning Initialisierung des arrays notwendig!
+        public struct tsRawAdcValuesResponse
+        {
+            public tsRawAdcValBuff sRawAdcValBuff;
+            public byte ucDlbIdx;
+        };
+    }
+}
