32285ebcaed12ebb4e2a258c096b790b446de646
Kraemere <Kraemere@31f6ca0a-dfc5-7646-ac17-90a69fb9f71f>
2020-03-01 12:14:29 +0000

    Added comments
    git-svn-id: file:///C/Users/kraemere/OneDrive/SVN-Repos/VisualStudio-Projects/MessageLogger/trunk@8 31f6ca0a-dfc5-7646-ac17-90a69fb9f71f

--- MessageLoggerForm/Form1.cs
+++ MessageLoggerForm/Form1.cs
@@ -17,6 +17,9 @@ namespace MessageLoggerForm
 {
     public partial class Form1 : Form
     {
+        /****************************************************************************************************
+        * C-DLL functions
+        ****************************************************************************************************/
         [DllImport("MessageLib.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern int SumVariables(int a, int b);
 
@@ -29,10 +32,14 @@ namespace MessageLoggerForm
         [DllImport("MessageLib.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern void InitModule();
 
+
+
+        /****************************************************************************************************
+        * Variables
+        ****************************************************************************************************/
         const UInt16 uiBaudRate = 38400;
 
         //Messages
-        private Class_Message sMsg = new Class_Message();
         private UInt32 ulMsgIdxCnt = 0;
 
         private ListViewItem lvItem;
@@ -43,24 +50,36 @@ namespace MessageLoggerForm
         private string MsgFrame;
 
 
+        /****************************************************************************************************
+        * Functions of the FORM
+        ****************************************************************************************************/
         public Form1()
         {
             InitializeComponent();
 
             //Init combo box 
-            getAvailablePorts();
+            GetAvailablePorts();
 
             InitModule();
         }
 
-        //Function to get all available Ports and display them in the combo box
-        void getAvailablePorts()
+        /****************************************************************************************************
+        * @brief: Reads all available ports and put them into the combo-box.
+        * @param: none
+        * @return: none
+        ****************************************************************************************************/
+        void GetAvailablePorts()
         {
             String[] sPorts = SerialPort.GetPortNames();
             ComboBoxSerialComPorts.Items.AddRange(sPorts);
         }
 
-        //Function to start the COM port
+        /****************************************************************************************************
+          * @brief: Event handler for the "COM-Port-START" button. Checks if a COM-Port is selected and opens
+          *         it with the predefined settings.
+          * @param: none
+          * @return: none
+          ****************************************************************************************************/
         private void BtnComPortStart_Click(object sender, EventArgs e)
         {
             try
@@ -86,6 +105,13 @@ namespace MessageLoggerForm
             }
         }
 
+
+
+        /****************************************************************************************************
+         * @brief: Event handler for the "COM-Port-STOP" button. Closes the serial port.
+         * @param: none
+         * @return: none
+         ****************************************************************************************************/
         private void BtnComPortStop_Click(object sender, EventArgs e)
         {
             try
@@ -100,35 +126,56 @@ namespace MessageLoggerForm
             }
         }
 
+
+
+        /****************************************************************************************************
+         * @brief: Adds the text box with the serial data in hex-format
+         * @param: none
+         * @return: none
+         ****************************************************************************************************/
         private void AddTextToSerialDataTextBox()
         {
+            //Put the time index at the first place
             DateTime sDate = DateTime.Now; 
             RichTextBoxSerialData.Text += sDate.TimeOfDay.ToString() + " ";
+
+            //Put the whole buffer data into the text box
             for (byte Idx = 0; Idx < BufferCnt; Idx++)
             {
-                string sBufferEntry = aucBuffer[Idx].ToString("X2");
+                string sBufferEntry = this.aucBuffer[Idx].ToString("X2");
                 
                 RichTextBoxSerialData.Text += sBufferEntry + " ";
             }
-
             RichTextBoxSerialData.Text += "\n";
         }
 
+
+
+        /****************************************************************************************************
+         * @brief: Adds the new created list view item at the first position
+         * @param: none
+         * @return: none
+         ****************************************************************************************************/
         private void AddNewItem()
         {
             listView1.Items.Insert(0, this.lvItem);
         }
 
+
+
+        /****************************************************************************************************
+         * @brief: Fills an array for the list view with the interprated data.
+         * @param: sMsgFrame - The message frame which shall be interprated and put into the list view
+         * @return: none
+         ****************************************************************************************************/
         private unsafe void FillListView(Class_Message.tsMessageFrame sMsgFrame)
         {
             /* Create string array with the count of list view colums */
             string[] asMsgArray = new string[listView1.Columns.Count];
 
-            
             DateTime sDate = DateTime.Now;
             Class_Interpreter Interpreter = new Class_Interpreter();
             
-
             /* Add items to list view */
             asMsgArray[0] = this.ulMsgIdxCnt.ToString();
             ++this.ulMsgIdxCnt;
@@ -151,39 +198,47 @@ namespace MessageLoggerForm
             asMsgArray[7] = Interpreter.InteprateID(sMsgFrame.sPayload.ucMsgId);
             asMsgArray[8] = Interpreter.InteprateCommand(sMsgFrame.sPayload.ucCommand);
 
+            /* Create new list view item to enable the adding */
             this.lvItem = new ListViewItem(asMsgArray);
 
+            /* Call per invoke to put the item to the list view */
             this.Invoke(new MethodInvoker(AddNewItem));            
         }
 
 
+
+        /****************************************************************************************************
+         * @brief: Interrupt handler on received data. The serial port is read and cleared afterwards. 
+         *         Puts the received data into the text box and fills the list view when a message is constructed.
+         * @param: sender - The serial port which started this event
+         * @return: none
+         ****************************************************************************************************/
         private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             SerialPort sp = (SerialPort)sender;
-
-            //const int siTryToRead = 255;
-            //byte[] aucBuffer = new byte[siTryToRead];
             this.BufferCnt = (byte)sp.BytesToRead;
 
             try
             {
-                sp.Read(aucBuffer, 0, BufferCnt);
+                //Read the serial port buffer
+                sp.Read(this.aucBuffer, 0, BufferCnt);
 
+                //Clear the serial port buffer to avoid an overfill
                 sp.DiscardInBuffer();
 
-                MsgLib_PutDataInBuffer(aucBuffer, BufferCnt);
+                //Put the buffer into the serial handling
+                MsgLib_PutDataInBuffer(this.aucBuffer, BufferCnt);
+
+                //Use invoke to put the whole received buffer into the text box
                 this.Invoke(new MethodInvoker(AddTextToSerialDataTextBox));
 
+                /* When a message was constructed with the serial-handling put
+                 * it into the list view */
                 Class_Message.tsMessageFrame sMsgFrame;
                 if(MsgLib_GetMessageFrame(out sMsgFrame))
                 {
-
                     FillListView(sMsgFrame);
-//                    DateTime sDate = DateTime.Now; 
-//                    MsgFrame = sDate.TimeOfDay.ToString() + " "+  sMsgFrame.sHeader.ucPreamble.ToString("X2") +" "+ sMsgFrame.sHeader.ucDestAddress.ToString("X2") +" "+ sMsgFrame.sHeader.ucSourceAddress.ToString("X2") + " " + sMsgFrame.sHeader.ucMsgType.ToString("X2") + "\n";
-                }
-
-                
+                }                
             }
             catch(Exception ex)
             {
